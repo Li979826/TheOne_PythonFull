1e8a067f55d64135965d9ac68dc7e4527e3e0eaf
Carl <291437965@qq.com>
2020-02-27 14:40:47 +0800

    每天的基础学习

---
+++ py07_字符串.py
@@ -0,0 +1,22 @@
+﻿#!/usr/bin/env python
+# encoding: utf-8
+'''
+
+@author: Carl
+        ｛源前｝
+@License: (C) Copyright 2013-2017, Node Supply Chain Manager Corporation Limited.
+@Contact: 520liyuan1314@gmail.com 源前
+          291437965@qq.com 
+@SoftWare: PyCharm
+@FileName: py07_字符串.py
+@Program：TheOne_PythonFull
+@Time: 2020/2/27 14:31
+@desc:
+'''
+s1='abddefg'
+s2='''123456'''
+s3="9785223"
+
+print(s1)
+print(s2)
+print(s3)
